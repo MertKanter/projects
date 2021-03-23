335c1107c9aa545a7537987e0ae4187c5c0dc186
MertKanter <81162983+MertKanter@users.noreply.github.com>
2021-03-23 12:14:21 +0300

    Add files via upload

---
+++ SecmeSecilmeHakki/kosullar1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kosullar1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Tekrar:
+
+            Console.WriteLine("Yaşınızı giriniz: ");
+            int yas = int.Parse(Console.ReadLine());
+            if (yas < 18)
+            {
+                Console.WriteLine("Seçme ve seçilme hakkınız yoktur");
+
+            }
+            else if (yas < 25)
+            {
+                Console.WriteLine("Seçme hakkınız vardır. Seçilme hakkınız yoktur.");
+            }
+            else
+
+            {
+                Console.WriteLine("Seçme ve seçilme hakkınız vardır.");
+
+            }
+
+            goto Tekrar;
+        }
+    }
+}
