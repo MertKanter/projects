431a03850560dfd144246786e85adb6e140ca11a
MertKanter <81162983+MertKanter@users.noreply.github.com>
2021-03-23 12:14:54 +0300

    Add files via upload

---
+++ TekCift/kosullar2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kosullar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tekrar:
+            Console.WriteLine("Bir sayı giriniz: ");
+            
+            int sayi = int.Parse(Console.ReadLine());
+
+            if (sayi%2==0)
+            {
+
+                Console.WriteLine($"Girilen Sayı {sayi} ve çifttir.");
+
+            }
+            else
+            {
+                Console.WriteLine($"Girilen Sayı {sayi} ve tektir.");
+            }
+            if (sayi!=0)
+            {
+                goto Tekrar;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
